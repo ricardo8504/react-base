e5b87f50457b73dea65aea72304db2a0f094060f
Ricardo Osorio <riardo.osorio@endava.com>
2024-10-11 10:26:19 -0500

    add linter changes for jest

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,13 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-
 export default [
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        jest: true,
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -33,6 +30,10 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      'no-unused-vars': ['error', { varsIgnorePattern: '^React$' }],
+    },
+    env: {
+      jest: true,
     },
   },
 ]
